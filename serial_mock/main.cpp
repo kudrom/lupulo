4a44b0c5863de8a250c3b35cd7b3f66343d6849d
kudrom <kudrom@riseup.net>
2015-09-17 15:55:03 +0200

    Changed the message of the serial mock

--- serial_mock/main.cpp
+++ serial_mock/main.cpp
@@ -6,9 +6,12 @@ Serial pc(USBTX, USBRX);
 int main()
 {
     pc.baud(115200);
+    char msg[] = {'A','B'};
+    int cnt = 0;
 
     while(1){
-        pc.putc('.');
+        pc.putc(msg[cnt%2]);
         wait(1);
+        cnt++;
     }
 }
