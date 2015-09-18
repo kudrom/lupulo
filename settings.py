f026258173c9424b097fb5b3cd00954f5e578085
kudrom <kudrom@riseup.net>
2015-09-18 11:27:01 +0200

    The web server is fully functional

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
+import os.path
+
 settings = {
         "serial_device": "/dev/ttyACM0",
-        "web_server_port" : 8080,
+        "web_server_port": 8080,
+        "templates_dir": os.path.abspath("templates"),
 }
--- webpage.py
+++ webpage.py
@@ -1,4 +1,9 @@
-from twisted.web import resource
+import os.path
+
+from twisted.web import resource, server
+from twisted.web.template import Element, renderer, XMLFile, flatten
+from twisted.python.filepath import FilePath
+from settings import settings
 
 
 class Root(resource.Resource):
@@ -8,21 +13,10 @@ class Root(resource.Resource):
         return resource.Resource.getChild(self, name, request)
 
     def render_GET(self, request):
-        return r"""
-        <html>
-            <head>
-                <script language="JavaScript">
-                        eventSource = new EventSource("http://localhost:8080/subscribe");
-                        eventSource.onmessage = function(event) {
-                            element = document.getElementById("event-data");
-                            element.innerHTML = event.data;
-                        };
-                    </script>
-            </head>
-            <body>
-                <h1> m3dpi demo</h1>
-                <h3> Event data: </h3>
-                <p id="event-data"></p>
-            </body>
-        </html>
-        """
+        d = flatten(request, RootElement(), request.write)
+        d.addCallback(lambda _, x: x.finish(), request)
+        return server.NOT_DONE_YET
+
+
+class RootElement(Element):
+    loader = XMLFile(FilePath(os.path.join(settings["templates_dir"], "index.html")))
