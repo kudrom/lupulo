2c172cb283aa22e3a6b8b5086aa1be79284a7c18
kudrom <kudrom@riseup.net>
2015-10-05 09:52:56 +0000

    The install script is working now

--- install.sh
+++ install.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/bash
 
-echo "THIS SCRIPT IS GUARANTED TO WORK ONLY ON ARCH DISTRIBUTIONS"
-command -v pip2 > /dev/null
+ID=`id -u`
+if [ $ID -ne 0 ]
+then
+    echo "You must be root in order to run this script."
+    exit -1
+fi
+
+command -v pacman > /dev/null
 if [ $? -eq 0 ]
 then
+    pacman --noconfirm -S base-devel
+
+    pacman --noconfirm -S mongodb
+    systemctl enable mongodb
+    systemctl start mongodb
+
+    pacman --noconfirm -S python2 python2-pip
     pip2 install -r requirements.txt
+
+    echo "INSTALLATION COMPLETE"
     echo "You should see the settings file before running the servers."
 else
-    echo "You need to install pip2 first."
+    echo "This script only works on arch distributions."
 fi
