a17f4b3bb000fbe9412b69f9d2afb9868bf3b69b
kudrom <kudrom@riseup.net>
2015-11-09 14:54:36 +0100

    added preconditions to add_alert

--- lupulo/defaults/static/js/alerts.js
+++ lupulo/defaults/static/js/alerts.js
@@ -1,5 +1,10 @@
 // Add an alert to the webpage
 function add_alert(type, text){
+    if(!(type in ['success', 'info', 'warning', 'danger'])){
+        text = "<strong>type</strong> parameter of <strong>add_alert</strong>" +
+               " is invalid.";
+        type = "danger";
+    }
     var button = '<button type="button" class="close" data-dismiss="alert" ' +
                  'aria-label="Close">' +
                      '<span aria-hidden="true">&times;</span>' +
